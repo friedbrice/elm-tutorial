ebbe7b01b2ce466d05cf9e822ccbaafe7c1094fc
Daniel Brice <danielbrice@gmail.com>
2018-08-18 22:29:02 -0700

    playing with the mock api server

--- api.js
+++ api.js
@@ -13,3 +13,9 @@ console.log('Listening at 4000')
 server.listen(4000)
 
 // run server with `node api.js`
+
+// make a GET request against a resource to query that resource
+// make a DELETE request against a resource to delete that resource
+// make a PUT request against a resource to replace that resource
+// make a PATCH request against a resource to update that resource
+// make a POST request against a resource to create a new child resource
